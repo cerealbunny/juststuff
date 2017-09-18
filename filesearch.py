67cd4b34d14b2b7110e60f8006231c3d2ea569f5
Michael Tang <michaeltang@Michaels-MBP.hsd1.ca.comcast.net>
2017-09-17 22:58:31 -0700

    uhh is it changed now

--- filesearch.py
+++ filesearch.py
@@ -1,27 +1,31 @@
-"""os.path.isdir() to determine if dir
-    if so move to dir
-    filecmp.cmp to compare two files
-    easiest shit in my life to do, but the trick is making it faster"""
+"""probably create additional helpers, this is no longer findtwin but find*twins
+    dictionary to store files now by filetype
+    then, depending on the filetype chosen, hash the entire list stored in dict entry by their file size. find the match."""
 
 import filecmp
 import os
 import os.path
 import re
-def childfiles(dir):
-    os.chdir(dir)
-    lst = list(map(lambda x:os.path.abspath(x),os.listdir()))
-    for file in lst:
-        if os.path.isdir(file):
-            for item in childfiles(file):
-                lst.append(item)
-    return lst
+def gendict(dir): #generates dictionary of all files in dir and under, key=filetype & value=list of all files of type
+    os.chdir('/')
+    wlst = os.walk(dir)
+    d = {'NoType':[]}
+    for tup in wlst:
+        os.chdir(tup[0])
+        for i in tup[2]:
+            ftype = re.search(r'\.[^\.]+$',i).group(0) if re.search(r'\.[^\.]+$',i) else None
+            if not ftype:
+                d['NoType'].append(os.path.abspath(i))
+            elif ftype not in d:
+                d[ftype] = [os.path.abspath(i)]
+            else:
+                d[ftype].append(os.path.abspath(i))
+    return d
 
-def findtwin(dir):
-    ftype = input('Input filetype (.xxx format), or press ENTER if no specific filetype desired: ')
+def findtwins():
     exitype = not not re.match(r'(\w*\n$)',ftype)
-    owd = os.getcwd()
-    lst = childfiles(dir)
-    os.chdir(owd); os.chdir(dir)
+    lst = gendict('/Users/michaeltang')
+    os.chdir('/')
     for _ in range(len(lst)):
         x = lst.pop()
         #print(lst)
@@ -36,5 +40,5 @@ def findtwin(dir):
                         print('----\n{}\nmatches\n{}\n----'.format(x, item))
 
 if __name__ == "__main__":
-    os.chdir('/Users/michaeltang')
-    findtwin(input('Input desired directory: '))
+    findtwins()
+
